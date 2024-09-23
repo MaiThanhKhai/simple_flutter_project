ab45320568791699a3a3bebae9708c1ac4583eb0
MaiThanhKhai <maithanhkhai2004tb@gmail.com>
2024-09-23 18:37:54 +0700

    Tiếp tục làm frontend và backend

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -12,7 +12,7 @@ final _router = Router(notFoundHandler: _notFoundHandle)
   ..get('/api/v1/echo/<message>', _echoHandler)
   ..post('/api/v1/submit', _submitHandler);
 
-final _headers = {'ContentType': 'application/json'};
+final _headers = {'Content-Type': 'application/json'};
 
 Response _notFoundHandle(Request req) {
   return Response.notFound(
@@ -50,7 +50,7 @@ Future<Response> _submitHandler(Request req) async {
         json.encode(response),
         headers: _headers,
       );
-    }else{
+    } else {
       final response = {'message': 'Server khong nhan duoc ten cua ban'};
       return Response.badRequest(
         body: json.encode(response),
@@ -70,12 +70,32 @@ void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
+  final corsHeader = createMiddleware(requestHandler: (req) {
+    if (req.method == 'OPTION') {
+      return Response.ok('', headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, HEAD',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      });
+    }
+    return null;
+  }, responseHandler: (res) {
+    return res.change(headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, HEAD',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+    });
+  });
+
   // Configure a pipeline that logs requests.
   final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+      Pipeline()
+      .addMiddleware(corsHeader)
+      .addMiddleware(logRequests())
+      .addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  print('Server dang chay tai http://${server.address.host} ${server.port}');
 }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+//import 'dart:io';
+
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +15,78 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Ung dung full-stack flutter don gian',
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = TextEditingController();
+
+  String responseMessage = '';
+  
+
+  Future<void> sendName() async {
+    final name = controller.text;
+    controller.clear();
+
+    final url = Uri.parse('http://localhost:8080/api/v1/submit');
+    try {
+      final response = await http
+          .post(
+            url,
+            headers: {'Content-Type': 'application/json'},
+            body: json.encode({'name': name}),
+          )
+          .timeout(const Duration(seconds: 10));
+      if (response.body.isNotEmpty) {
+        final data = json.decode(response.body);
+        setState(() {
+          responseMessage = data['message'];
+        });
+      } else {
+        setState(() {
+          responseMessage = 'Khong nhan phan hoi tu server';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        responseMessage = 'Da xay ra loi: ${e.toString()}';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Ung dung full-stack flutter don gian')),
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(labelText: 'Ten'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FilledButton(onPressed: sendName, child: const Text('Gui')),
+              Text(
+                responseMessage,
+                style: Theme.of(context).textTheme.titleLarge,
+              )
+            ],
+          ),
+        ));
   }
 }
